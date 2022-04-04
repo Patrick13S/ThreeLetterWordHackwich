3d6c01b3c646c5f30e8d47ccd798d658b3ac5d8a
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-04-04 14:11:20 -0600

    3 outlets

--- ThreeLetterWordHackwich/ViewController.swift
+++ ThreeLetterWordHackwich/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    
+    @IBOutlet var wordLabels: [UILabel]!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func whenTapped(_ sender: Any)
+    {
+        
+        
+    }
+    
 }
 
