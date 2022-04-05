33bd45fde4df15bf556a482c5c15c9b9c89ed3ac
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-04-05 14:13:35 -0600

    for loop

--- ThreeLetterWordHackwich/ViewController.swift
+++ ThreeLetterWordHackwich/ViewController.swift
@@ -17,17 +17,38 @@ class ViewController: UIViewController
     @IBOutlet weak var stackView: UIStackView!
     
     
+    var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
+    var counter = 0
+    var currentLetter = "A"
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
     }
 
     
     @IBAction func whenTapped(_ sender: Any)
     {
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+        
+        let selectedPoint = ((sender as! AnyObject).location(in: stackView))
+        
+    
+        for label in wordLabels
+        {
+        if label.frame.contains(selectedPoint)
+            {
+        label.text = "\(currentLetter)"
+        }
+        }
+        
+        counter += 1
+    
         
         
     }
